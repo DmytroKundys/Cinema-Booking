7bdaa38f584caed5a421691fca52777bad41be20
Dmytro bc2024-2 <dkundys2.0@gmail.com>
2025-05-18 18:08:40 +0300

    додано компонент MovieCard для відображення фільму

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./MovieCard.css";
+
+function MovieCard({ movie }) {
+  return (
+    <div className="movie-card">
+      <img src={movie.image} alt={movie.title} className="movie-poster" />
+      <h2 className="movie-title">{movie.title}</h2>
+      <p className="movie-description">{movie.description}</p>
+      <p className="movie-genre">{movie.genre}</p>
+      <p className="movie-time">Сеанс: {movie.time}</p>
+    </div>
+  );
+}
+
+export default MovieCard;
