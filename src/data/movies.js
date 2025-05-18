0461fb8b4d127e23a5197270d697f746b3172db0
Dmytro bc2024-2 <dkundys2.0@gmail.com>
2025-05-18 17:39:01 +0300

    Add data file with initial movie list

---
+++ src/data/movies.js
@@ -0,0 +1,27 @@
+export const movies = [
+    {
+      id: 1,
+      title: "Interstellar",
+      description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+      genre: "Science Fiction",
+      image: "https://m.media-amazon.com/images/I/91kFYg4fX3L._AC_SL1500_.jpg",
+      time: "18:00"
+    },
+    {
+      id: 2,
+      title: "Inception",
+      description: "A thief who steals corporate secrets through dream-sharing technology is given the task of planting an idea.",
+      genre: "Action/Thriller",
+      image: "https://m.media-amazon.com/images/I/81p+xe8cbnL._AC_SY679_.jpg",
+      time: "20:00"
+    },
+    {
+      id: 3,
+      title: "The Matrix",
+      description: "A computer hacker learns about the true nature of his reality and his role in the war against its controllers.",
+      genre: "Cyberpunk",
+      image: "https://m.media-amazon.com/images/I/51vpnbwFHrL._AC_.jpg",
+      time: "22:00"
+    }
+  ];
+  
